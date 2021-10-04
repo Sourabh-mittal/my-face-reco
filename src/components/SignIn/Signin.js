57d0b7a6d626c104d533e5038138f61ef3d0c73c
Sourabh-mittal <myfavasta@gmail.com>
2021-10-04 08:49:09 +0530

    sign in button styles change

--- src/components/SignIn/Signin.js
+++ src/components/SignIn/Signin.js
@@ -82,7 +82,7 @@ class Signin extends React.Component {
             <div className="">
               <input
                 onClick={this.onSubmitSignIn}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f5 dib"
                 type="submit"
                 value="Sign in"
               />
