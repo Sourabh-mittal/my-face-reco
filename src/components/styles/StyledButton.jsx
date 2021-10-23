549759083b26c4de48b1b2ed6918c74c57b86cef
Sourabh-mittal <myfavasta@gmail.com>
2021-10-23 10:42:13 +0530

    sfdsd

--- src/components/styles/StyledButton.jsx
+++ src/components/styles/StyledButton.jsx
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 export const StyledButton = styled.button`
   border: 2px solid #333;
-  border-radius: 4px;
+  border-radius: 5em;
   background: linear-gradient(89deg, #ff5edf 0%, #04c8de 100%);
   font-size: 20px;
   margin: 10px;
-  padding: 10px 5px;
+  padding: 10px;
   transition: all 0.5s ease-in-out;
 
   &:hover {
